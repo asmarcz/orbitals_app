d9817aaa97f9be561d46533fd3d43084b1dd78e4
asmar <oliver.tusla@seznam.cz>
2019-02-24 11:49:22 +0100

    assets, bootstrap, vue.js, model.js, schrodinger.js

---
+++ www/js/model.js
@@ -0,0 +1,151 @@
+function mathModel(k, l) {
+	return l + k * (k - 1) / 2;
+}
+
+function calcOrientationNumber(l) {
+	return l * 2 + 1;
+}
+
+function maxOrbitalElectronNumber(type) {
+	return calcOrientationNumber(type) * 2;
+}
+
+function getOrbitalTypeText(number) {
+	switch (number) {
+		case 0:
+			return 's';
+		case 1:
+			return 'p';
+		case 2:
+			return 'd';
+		case 3:
+			return 'f';
+		case 4:
+			return 'g';
+		default:
+			return number;
+	}
+}
+
+class Controller {
+	constructor(element, quantumLayer, exceptions) {
+		/** @type QuantumLayer[] */
+		this.layerCollection = [quantumLayer];
+		this.element = element;
+		this.maxN = quantumLayer.n;
+		this.exceptions = exceptions;
+	}
+
+	createQuantumLayer() {
+		this.maxN++;
+		this.layerCollection.push(new QuantumLayer(this.maxN));
+	}
+
+	hasMore() {
+		let lastOrbital = this.element.orbitals[this.element.orbitals.length - 1];
+		let toSubtract = this.element.countNumber - this.element.electronNumber;
+		lastOrbital.electronNumber -= toSubtract;
+		this.element.countNumber -= toSubtract;
+	}
+
+	initiate() {
+		this.createQuantumLayer();
+		while (this.element.electronNumber !== this.element.countNumber) {
+			if (this.element.electronNumber < this.element.countNumber) {
+				this.hasMore();
+			} else {
+				let newestLayer, secondNewestLayer;
+				let secondMaxN = this.maxN - 1, collectionLength = this.layerCollection.length;
+				for (let i = 0; i < collectionLength; i++) {
+					if (this.layerCollection[i].n === secondMaxN) {
+						secondNewestLayer = this.layerCollection[i];
+						newestLayer = this.layerCollection[i + 1];
+						break;
+					}
+				}
+				if (newestLayer.lState === 0 && secondNewestLayer.lState !== 0) {
+					this.createQuantumLayer();
+				}
+
+				this.layerCollection.sort((first, second) => {
+					let result = first.sum - second.sum;
+					if (result === 0) {
+						return first.n - second.n;
+					}
+					return result;
+				});
+
+				for (let layer of this.layerCollection) {
+					if (layer.canBeUsed()) {
+						this.element.addOrbital(layer.lState, layer.n);
+						layer.lState++;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	checkExceptions() {
+		let lastDExceptionOrbital = this.element.orbitals.find((orbital) => {
+			return orbital.type === 2 && (orbital.electronNumber === 4 || orbital.electronNumber === 9);
+		});
+		if (typeof lastDExceptionOrbital !== 'undefined') {
+			let lastSOrbital = this.element.orbitals.slice().reverse().find((orbital) => {
+				return orbital.type === 0;
+			});
+			lastSOrbital.electronNumber--;
+			lastDExceptionOrbital.electronNumber++;
+		}
+
+		if (this.element.electronNumber in this.exceptions) {
+			let orbitals = new Array(this.element.orbitals.length)
+			for (let orbital of this.element.orbitals) {
+				orbitals[mathModel(orbital.n, orbital.type)] = orbital
+			}
+
+			for (let index in this.exceptions[this.element.electronNumber]) {
+				orbitals[parseInt(index)].electronNumber = this.exceptions[this.element.electronNumber][index]
+			}
+		}
+	}
+}
+
+class Orbital {
+	constructor(type, n, electronNumber = false) {
+		this.type = type;
+		this.n = n;
+		this.electronNumber = electronNumber ? electronNumber : maxOrbitalElectronNumber(type);
+	}
+}
+
+class Element {
+	constructor(protonNumber) {
+		this.protonNumber = protonNumber;
+		this.electronNumber = protonNumber;
+		this.countNumber = 0;
+		/** @type Orbital[] */
+		this.orbitals = [];
+	}
+
+	addOrbital(type, n) {
+		let orbital = new Orbital(type, n);
+		this.orbitals.push(orbital);
+		this.countNumber += orbital.electronNumber;
+	}
+}
+
+class QuantumLayer {
+	constructor(n) {
+		this.n = n;
+		this.lState = 0;
+	}
+
+	canBeUsed() {
+		return this.n > this.lState;
+	}
+
+	get sum() {
+		return this.n + this.lState;
+	}
+}
---
+++ www/js/schrodinger.js
@@ -0,0 +1,50 @@
+function factorial(num) {
+	if (num < 0)
+		return -1;
+	else if (num === 0)
+		return 1;
+	else {
+		return (num * factorial(num - 1));
+	}
+}
+
+function schrodinger(n, l, m, step = 0.1, radius = 0.1) {
+	let output = []
+	let info = []
+	for (let theta = 0; theta < 2 * Math.PI; theta += step) {
+		for (let phi = 0; phi < 2 * Math.PI; phi += step) {
+			for (let r = 0; r <= 30; r += radius) {
+				let schro1 = (-1) ** ((m + Math.abs(m) + 2) / 2) * (1 / (n ** (l + 2))) * Math.sqrt((factorial(n - l - 1) * factorial(n + l) * factorial(l - Math.abs(m)) * (2 * l + 1)) / (Math.PI * factorial(l + Math.abs(m))))
+				let schro2 = (r ** l) * (Math.E ** (-r / n))
+				let schro3 = 0
+				for (let j = 0; j <= (n - l - 1); j++) {
+					schro3 += (((-2 * r) / n) ** (n - l - j - 1)) / (factorial(n + l - j) * factorial(j) * factorial(n - l - j - 1))
+				}
+				let schro4 = Math.sin(theta) ** (Math.abs(m))
+				let schro5 = 0
+				for (let j = 0; j <= ((l - Math.abs(m)) / 2); j++) {
+					schro5 += ((((-1) ** j) * factorial(2 * l - 2 * j)) / (factorial(j) * factorial(l - j) * factorial(l - Math.abs(m) - 2 * j))) * (Math.cos(theta) ** (l - Math.abs(m) - 2 * j))
+				}
+				let schro6
+				if (m > 0) {
+					schro6 = Math.sqrt(2) * Math.sin(m * phi)
+				} else if (m === 0) {
+					schro6 = 1
+				} else {
+					schro6 = Math.sqrt(2) * Math.cos(m * phi)
+				}
+				let schro = schro1 * schro2 * schro3 * schro4 * schro5 * schro6
+				schroSquared = schro ** 2
+				if (schroSquared > 10 ** (-5)) {
+					let sinTheta = Math.sin(theta)
+					let x = r * sinTheta * Math.cos(phi)
+					let y = r * sinTheta * Math.sin(phi)
+					let z = r * Math.cos(theta)
+					output.push([x, y, z])
+					info.push(schro > 0 ? 1 : 0)
+				}
+			}
+		}
+	}
+	return [output, info]
+}

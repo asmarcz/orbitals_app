7232c42bfecb03cb39f91139eb4826ffad419445
asmar <oliver.tusla@seznam.cz>
2019-05-27 22:39:17 +0200

    few loop schrodinger optimizations

--- www/js/schrodinger.js
+++ www/js/schrodinger.js
@@ -14,33 +14,34 @@ function schrodinger(n, l, m, step = 0.1, radius = 0.1) {
 	let xs = []
 	let ys = []
 	let zs = []
+
+	let schro1 = (-1) ** ((m + Math.abs(m) + 2) / 2) * (1 / (n ** (l + 2))) * Math.sqrt((factorial(n - l - 1) * factorial(n + l) * factorial(l - Math.abs(m)) * (2 * l + 1)) / (Math.PI * factorial(l + Math.abs(m))))
 	for (let theta = 0; theta < Math.PI; theta += step) {
+		let schro4 = Math.sin(theta) ** (Math.abs(m))
+		let schro5 = 0
+		for (let j = 0; j <= ((l - Math.abs(m)) / 2); j++) {
+			schro5 += ((((-1) ** j) * factorial(2 * l - 2 * j)) / (factorial(j) * factorial(l - j) * factorial(l - Math.abs(m) - 2 * j))) * (Math.cos(theta) ** (l - Math.abs(m) - 2 * j))
+		}
+		let sinTheta = Math.sin(theta)
 		for (let phi = 0; phi < 2 * Math.PI; phi += step) {
+			let schro6
+			if (m > 0) {
+				schro6 = Math.sqrt(2) * Math.sin(m * phi)
+			} else if (m === 0) {
+				schro6 = 1
+			} else {
+				schro6 = Math.sqrt(2) * Math.cos(m * phi)
+			}
 			for (let r = 0; r <= 80; r += radius) {
-				let schro1 = (-1) ** ((m + Math.abs(m) + 2) / 2) * (1 / (n ** (l + 2))) * Math.sqrt((factorial(n - l - 1) * factorial(n + l) * factorial(l - Math.abs(m)) * (2 * l + 1)) / (Math.PI * factorial(l + Math.abs(m))))
 				let schro2 = (r ** l) * (Math.E ** (-r / n))
 				let schro3 = 0
 				for (let j = 0; j <= (n - l - 1); j++) {
 					schro3 += (((-2 * r) / n) ** (n - l - j - 1)) / (factorial(n + l - j) * factorial(j) * factorial(n - l - j - 1))
 				}
-				let schro4 = Math.sin(theta) ** (Math.abs(m))
-				let schro5 = 0
-				for (let j = 0; j <= ((l - Math.abs(m)) / 2); j++) {
-					schro5 += ((((-1) ** j) * factorial(2 * l - 2 * j)) / (factorial(j) * factorial(l - j) * factorial(l - Math.abs(m) - 2 * j))) * (Math.cos(theta) ** (l - Math.abs(m) - 2 * j))
-				}
-				let schro6
-				if (m > 0) {
-					schro6 = Math.sqrt(2) * Math.sin(m * phi)
-				} else if (m === 0) {
-					schro6 = 1
-				} else {
-					schro6 = Math.sqrt(2) * Math.cos(m * phi)
-				}
 				let schro = schro1 * schro2 * schro3 * schro4 * schro5 * schro6
 				schroSquared = schro ** 2
 				psis.push(schroSquared)
 
-				let sinTheta = Math.sin(theta)
 				let x = r * sinTheta * Math.cos(phi)
 				let y = r * sinTheta * Math.sin(phi)
 				let z = r * Math.cos(theta)
@@ -54,10 +55,10 @@ function schrodinger(n, l, m, step = 0.1, radius = 0.1) {
 
 	let maxPsi = psis.reduce((a, b) => Math.max(a, b))
 	// David Manthey, Orbital Viewer, https://github.com/manthey/orbitalviewer/blob/aa49e320a1d52e49d4684d30ca8036b70f25c6da/ov.c#L2003-L2016
-	let threshold = Math.log10(maxPsi) - 1 - 0.25 * (n - l)
+	let threshold = 10 ** (Math.log10(maxPsi) - 1 - 0.25 * (n - l))
 	let result = [[], []]
 	for (let i = 0; i < xs.length; i++) {
-		if (psis[i] > 10 ** threshold) {
+		if (psis[i] > threshold) {
 			result[0].push(xs[i], ys[i], zs[i])
 			result[1].push(info[i])
 		}

8673736021e588aa97d5f7e578754580f30dc638
asmar <oliver.tusla@seznam.cz>
2020-02-05 21:05:36 +0100

    use path.join; dev mode; missing vars as warning; watching for templates

--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,34 @@
 const crypto = require("crypto")
 const fs = require("fs")
+const path = require("path")
 const babel = require("@babel/core")
+const watch = require("node-watch")
+
+let isDev = false
+let WVars = false
+
+for (let opt of process.argv) {
+	switch (opt) {
+		case "--dev":
+			isDev = true
+			break
+		case "--WVars":
+			WVars = true
+			break
+	}
+}
+
+function warning(message) {
+	console.log("W: " + message)
+}
+
+function updating(name) {
+	console.log("U: " + name)
+}
+
+function removing(name) {
+	console.log("R: " + name)
+}
 
 let time = ""
 {
@@ -10,27 +38,27 @@ let time = ""
 	}
 }
 
-const jsSrc = __dirname + "/../src/js"
-const jsDist = __dirname + "/../www/js"
-const cssSrc = __dirname + "/../src/css"
-const cssDist = __dirname + "/../www/css"
-const htmlSrc = __dirname + "/../src/templates"
-const htmlDist = __dirname + "/../www"
+const jsSrc = path.join(__dirname, "..", "src", "js")
+const jsDist = path.join(__dirname, "..", "www", "js")
+const cssSrc = path.join(__dirname, "..", "src", "css")
+const cssDist = path.join(__dirname, "..", "www", "css")
+const htmlSrc = path.join(__dirname, "..", "src", "templates")
+const htmlDist = path.join(__dirname, "..", "www")
 
 const defaults = {
 	scripts: {},
 	styles: {},
 }
 
-function getOld(directory, prefix) {
+function getOld(directory, prefix, content = true) {
 	const old = {
 		content: "",
 	}
 	for (let file of fs.readdirSync(directory)) {
 		if (file.startsWith(prefix)) {
 			old.filename = file
-			old.path = directory + "/" + file
-			old.content = fs.readFileSync(old.path, "utf-8")
+			old.path = path.join(directory, file)
+			if (content) old.content = fs.readFileSync(old.path, "utf-8")
 			break
 		}
 	}
@@ -50,16 +78,18 @@ function isDiff(freshContent, oldContent) {
 function diffIt(fresh, old) {
 	if (isDiff(fresh.content, old.content)) {
 		if (typeof old.path !== "undefined") {
+			removing(old.path)
 			fs.unlinkSync(old.path)
 		}
+		updating(fresh.path)
 		fs.writeFileSync(fresh.path, fresh.content)
 		return true
 	}
 	return false
 }
 
-let data = [
-	{
+let scheme = {
+	scripts: {
 		names: ["common", "homepage", "model", "OrbitControls", "schrodinger", "testpage", "Visualization"],
 		dist: jsDist,
 		src: jsSrc,
@@ -75,14 +105,14 @@ let data = [
 			},
 		},
 	},
-	{
+	styles: {
 		names: ["common", "homepage", "testpage"],
 		dist: cssDist,
 		src: cssSrc,
 		ext: "css",
 		vars: defaults.styles,
 	},
-	{
+	config: {
 		names: ["config"],
 		dist: jsDist,
 		ext: "js",
@@ -90,15 +120,15 @@ let data = [
 		on: {
 			fresh: function (fresh) {
 				for (let name of ["exceptions", "elements", "ranges", "layers", "nobleGases"]) {
-					fresh.content += `const ${name}=` + fs.readFileSync(__dirname + `/../config/${name}.json`, "utf-8") + ";"
+					fresh.content += `const ${name}=` + fs.readFileSync(path.join(__dirname, "..", "config", `${name}.json`), "utf-8") + ";"
 				}
 			},
 		},
 	},
-]
+}
 
-for (let i = 0; i < data.length; i++) {
-	let {dist, ext, names, on, src, vars} = data[i]
+for (let key in scheme) {
+	let {dist, ext, names, on, src, vars} = scheme[key]
 	for (let prefix of names) {
 		const old = getOld(dist, prefix)
 		if (typeof on !== "undefined" && typeof on.old !== "undefined") {
@@ -107,9 +137,9 @@ for (let i = 0; i < data.length; i++) {
 
 		const fresh = {}
 		fresh.filename = prefix + "_" + time + "." + ext
-		fresh.path = dist + "/" + fresh.filename
-		if (fs.existsSync(src + "/" + prefix + "." + ext)) {
-			fresh.content = fs.readFileSync(src + "/" + prefix + "." + ext, "utf-8")
+		fresh.path = path.join(dist, fresh.filename)
+		if (typeof src !== "undefined" && fs.existsSync(path.join(src, prefix + "." + ext))) {
+			fresh.content = fs.readFileSync(path.join(src, prefix + "." + ext), "utf-8")
 		} else {
 			fresh.content = ""
 		}
@@ -122,14 +152,6 @@ for (let i = 0; i < data.length; i++) {
 }
 
 {
-	Object.assign(defaults, {
-		activeHome: "",
-		activeTest: "",
-		activeAbout: "",
-		style: "",
-		script: "",
-	})
-
 	function extractVars(template) {
 		let ret = {}
 		let regExp = /%> ([a-zA-Z]+) <%([\s\S]*?)%> \1 <%/g
@@ -156,7 +178,12 @@ for (let i = 0; i < data.length; i++) {
 					}
 				}
 				if (typeof value === "undefined" || error) {
-					throw new Error(`Variable ${match[1]} doesn't exist.`)
+					if (WVars) {
+						value = ""
+						warning(`Variable ${match[1]} doesn't exist. Setting to empty string.`)
+					} else {
+						throw new Error(`Variable ${match[1]} doesn't exist.`)
+					}
 				}
 				template = template.replace("<% " + match[1] + " %>", value)
 				matched = true
@@ -165,12 +192,46 @@ for (let i = 0; i < data.length; i++) {
 		return template
 	}
 
-	let pages = ["index.html", "test.html", "about.html"]
-	let layout = fs.readFileSync(htmlSrc + "/layout.html", "utf-8")
-	for (let name of pages) {
-		let sub = fs.readFileSync(htmlSrc + "/" + name, "utf-8")
+	const layout = {}
+	layout.filename = "layout.html"
+	layout.path = path.join(htmlSrc, layout.filename)
+	layout.content = fs.readFileSync(layout.path, "utf-8")
+
+	function template(name) {
+		let sub = fs.readFileSync(name, "utf-8")
 		let vars = Object.assign(JSON.parse(JSON.stringify(defaults)), extractVars(sub))
-		let fresh = replaceVars(layout, vars)
-		fs.writeFileSync(htmlDist + "/" + name, fresh)
+		let fresh = replaceVars(layout.content, vars)
+		let newPath = path.join(htmlDist, path.basename(name))
+		updating(newPath)
+		fs.writeFileSync(newPath, fresh)
+	}
+
+	function allTemplates() {
+		for (let name of fs.readdirSync(htmlSrc)) {
+			if (name !== layout.filename) {
+				template(path.join(htmlSrc, name))
+			}
+		}
+	}
+
+	allTemplates()
+	if (isDev) {
+		watch(htmlSrc, function (evt, name) {
+			switch (evt) {
+				case "update":
+					if (name === layout.filename) {
+						allTemplates()
+					} else {
+						template(name)
+					}
+					break
+				case "remove":
+					const old = getOld(htmlDist, path.parse(name).name)
+					let newPath = path.join(htmlDist, old.filename)
+					removing(newPath)
+					fs.unlinkSync(newPath)
+					break
+			}
+		})
 	}
 }

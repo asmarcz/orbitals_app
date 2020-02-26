7828c0887640dc16d262a5c67a78f6c00fa33ebe
asmar <oliver.tusla@seznam.cz>
2020-02-26 22:55:15 +0100

    improve service worker for better updates

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,17 +1,25 @@
 // toCache gets generated here by build tool
-let cName = "orbitals.app"
+let cName = "orbitals.app" + Math.floor(Date.now() / 1000)
 
 self.addEventListener("install", (e) => {
 	self.skipWaiting()
 	e.waitUntil(
-		caches.delete(cName)
-			.then(() => {
-				caches.open(cName)
-					.then(c => {
-						c.addAll(toCache)
-					})
+		caches.open(cName)
+			.then(c => {
+				c.addAll(toCache)
 			})
 	)
+})
+
+self.addEventListener("activate", () => {
+	caches.keys()
+		.then(keys => {
+			for (let k of keys) {
+				if (k !== cName) {
+					caches.delete(k)
+				}
+			}
+		})
 })
 
 self.addEventListener("fetch", (e) => {
